122302522cc349039de4e5d32d7530a9b6164e2d
Priyanshu <82305798+Priyanshu2022@users.noreply.github.com>
2021-11-02 16:16:25 +0000

    some fixes

--- bot.js
+++ bot.js
@@ -494,7 +494,7 @@ var f=0;
 contestData.forEach(cur=>{
   if(cur.relativeTimeSeconds>0){
   if((cur.durationSeconds-cur.relativeTimeSeconds)>0){
-ans.push({"name":cur.name,"time":cur.startTimeSeconds});
+ans.push({"name":cur.name,"time":cur.relativeTimeSeconds});
 f=1;
   } 
   }
@@ -502,14 +502,16 @@ f=1;
 })
 // console.log(ans);
 // ans.reverse();
-
 const embed = new Discord.MessageEmbed().setColor(0x3498DB).setTitle('Running Contests');
 embed.setThumbnail('https://codeforces.org/s/29643/images/codeforces-logo-with-telegram.png')
 if(f===1){
   console.log("1")
+
 ans.forEach(cur=>{
-var d = new Date(cur.startTimeSeconds*1000);
+var d = new Date(Date.now()-cur.time*1000);
+console.log(d);
 var ti=d.toString().slice(0,24);
+console.log(ti);
 embed.addField(`${cur.name}`,`${ti}`,false)
 
 })
